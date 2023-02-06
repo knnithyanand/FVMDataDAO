21b3cbc41bbdf9d43a83de0004fc1edf08fb0f32
Nithyanand K N <knnithyanand@hotmail.com>
2023-02-06 23:42:43 +0800

    Renove location blocking logic

--- app/src/pages/Layout.tsx
+++ app/src/pages/Layout.tsx
@@ -1,21 +1,7 @@
-import React from "react";
-import { Outlet, useLocation } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 import { Footer, Navbar } from "../components";
 
 const Layout = () => {
-  let [setHistoryIndex] = React.useState(window.history.state?.idx);
-  let location = useLocation();
-
-  // Expose the underlying history index in the UI for debugging
-  React.useEffect(() => {
-    setHistoryIndex(window.history.state?.idx);
-  }, [location, setHistoryIndex]);
-
-  // Give us meaningful document titles for popping back/forward more than 1 entry
-  React.useEffect(() => {
-    document.title = location.pathname;
-  }, [location]);
-
   return (
     <>
       <Navbar />
