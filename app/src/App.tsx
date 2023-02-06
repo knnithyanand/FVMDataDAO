375892044b5fea6ac4c6949d7a9577860496ae5b
Nithyanand K N <knnithyanand@hotmail.com>
2023-02-06 23:03:41 +0800

    Remove BrowserRouter

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -5,18 +5,16 @@ import { Layout, Home, Market, NoPage, Details, Empty, Upload } from "./pages";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="market" element={<Market />} />
-          <Route path="details" element={<Details />} />
-          <Route path="empty" element={<Empty />} />
-          <Route path="upload" element={<Upload />} />
-          <Route path="*" element={<NoPage />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route path="market" element={<Market />} />
+        <Route path="details" element={<Details />} />
+        <Route path="empty" element={<Empty />} />
+        <Route path="upload" element={<Upload />} />
+        <Route path="*" element={<NoPage />} />
+      </Route>
+    </Routes>
   );
 }
 
