e775310b8516924ec5607aeecc6143cc4a7a79c5
Nithyanand K N <knnithyanand@hotmail.com>
2023-02-06 23:28:46 +0800

    Use createBrowserRouter in App

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,20 +1,27 @@
-import { Routes, Route } from "react-router-dom";
+import {
+  Route,
+  createRoutesFromElements,
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 import "./App.css";
 import { Layout, Home, Market, NoPage, Details, Empty, Upload } from "./pages";
 
+let router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<Layout />}>
+      <Route index element={<Home />} />
+      <Route path="market" element={<Market />} />
+      <Route path="details" element={<Details />} />
+      <Route path="empty" element={<Empty />} />
+      <Route path="upload" element={<Upload />} />
+      <Route path="*" element={<NoPage />} />
+    </Route>
+  )
+);
+
 function App() {
-  return (
-    <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route index element={<Home />} />
-        <Route path="market" element={<Market />} />
-        <Route path="details" element={<Details />} />
-        <Route path="empty" element={<Empty />} />
-        <Route path="upload" element={<Upload />} />
-        <Route path="*" element={<NoPage />} />
-      </Route>
-    </Routes>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
